1532ccc24ae556d882c2794a6774249f447021da
Sebastian Aguilera Velozo <sebastian.aguileraE@hotmail.com>
2022-09-27 22:23:09 -0300

    Agrego URL de Backend

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
 const STORAGE_NAME = "CART";
 const STORAGE_DEFAULT_VALUE = [];
 const MAX_STOCK = 12;
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://bsale-store-back.herokuapp.com';
